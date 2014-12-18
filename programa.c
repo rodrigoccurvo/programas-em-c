265fece060f07bd1984e6a3e998d5747576746ea
Rodrigo <rodrigoccurvo@gmail.com>
2014-12-18 09:34:30 -0200

    Incluido pergunta sobre idade.

--- programa.c
+++ programa.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
 int main() {
+    int idade;
+
     printf("Olá, mundo!");
+
+    printf("Qual sua idade?")
+    scanf("%d", &idade);
+
+    printf("Voce tem %d anos", idade);
     return 0;
 }
